b10af313a99d0a3a2109543061ef8e780ec229e7
Luca <l.calabrese@educasoftware.it>
2024-03-24 19:47:50 +0100

    first

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logoImg from '../assets/investment-calculator-logo.png';
+
+const Header = function() {
+  return (
+    <header>
+      <img src={logoImg} alt="projlogo"/>
+      <h1>Investment calculator</h1>
+    </header>
+  );
+};
+
+export default Header;
